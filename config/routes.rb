f68b1443373ccfd26e790671b28c631d3f979d26
stevegriffin <stephenmgriffin80@gmail.com>
2014-12-31 11:10:09 -0500

    added signup capabilies

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
 
   get 'home' => 'home#home', :as => 'home'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
