705c47a07d44af85e5ca5f022d2b068cfffa4048
stevegriffin <stephenmgriffin80@gmail.com>
2015-01-05 12:35:50 -0500

    added date and time formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  #default date format: M / D / Y
+  Date::DATE_FORMATS[:default] = '%m/%d/%Y'
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
